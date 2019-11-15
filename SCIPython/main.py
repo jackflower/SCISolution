5ad15fb96eb7efb8a45be615cbc2a1eaffc0ed4b
jackflower <jackflower@poczta.onet.pl>
2019-11-15 11:05:08 +0100

    Updat

--- SCIPython/main.py
+++ SCIPython/main.py
@@ -15,3 +15,17 @@ zielona = Tea.Tea()
 print(czarna)
 print(zielona)
 
+tablica = []
+tablica.append(1)
+tablica.append(2)
+tablica.append(3)
+
+#tablica tab 6-cio elementowa
+tab = [1, 23, 4, 6, 12, 7]
+print(tab)
+#zmieniamy wartość trzeciego elementu (4) na wartość -58
+tab[2] = -58
+print(tab)
+
+
+#tablica = 
--- SCISolution/main.cpp
+++ SCISolution/main.cpp
@@ -4,6 +4,27 @@
 
 int main()
 {
+	//tablica na 10 elementów typu int
+	int tablica[10];
+
+	//tablica na pięć elementów typu int
+	//wraz z ich zainicjowaniem wartościami
+	//podanymi w nawiasach klamrowych
+	int tab[5] = { 1, 45, -6, 12, 78 };
+
+	//deklaracja rozmiaru tablicy
+	//musi być const
+	const int rozmiar = 100;
+	//tablica o takim rozmiarze
+	//(zapis w nawiasach klamrowych
+	//spowoduje, że wszystkie elementy tablicy
+	//będą miały wartość 0
+	int centurion[rozmiar] = { 0 };
+
+	//elementowi ósmemu - pod indeksem siódmym
+	//nadajemy wartość 123
+	centurion[7] = 123;
+
 
 	int liczba = 10;
 	doubling(liczba);//wywołanie funckji z argumentem o wartości 10
