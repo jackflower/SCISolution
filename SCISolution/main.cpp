c23dc43ff1e72de71cf58655972f0c6cb9aa5cae
jackflower <jackflower@poczta.onet.pl>
2020-02-06 19:22:41 +0100

    Order in the source code

--- SCISolution/main.cpp
+++ SCISolution/main.cpp
@@ -1,13 +1,9 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <string>
-#include "source/class_demos/Bicykle.h"
-#include "source/class_demos/SuperBicykle.h"
 
 int main()
 {
-	//tutaj testujemy elementy kodu...
-
 	getchar();
 	return 0;
 }
